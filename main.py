1363b6441fc1a26220ed26aede0824579023e8ad
mesut.mutlu <mesut_mutlu83@yahoo.fr>
2023-01-03 08:38:09 +0300

    --

--- main.py
+++ main.py
@@ -52,6 +52,7 @@ if __name__ == '__main__':
             ind_l = ind_lb[ind_lb['db_id'] == db_id]['Unnamed: 0'].to_list()
             a = wb.data.DataFrame(ind_l, time=range(2000, 2022), skipBlanks=True,
                         columns='series').reset_index()
+            a.to_excel(str(wb.db)+'.xlsx')
             print('success db_id: ', str(db_id))
         except:
             print('error db_id: ', str(db_id))
